66dccd50bc2dced0cd2e77dfae896ea522d9f696
hocine bouretaa <hocinewiwoz1994@gmail.com>
2025-11-14 20:53:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -700,6 +700,8 @@ def security_txt():
     return "Contact: admin@dzhosing.serveftp.com\nPolicy: none", 200, {"Content-Type": "text/plain"}
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
